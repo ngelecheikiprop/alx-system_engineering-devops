4723231796c013a0310c28b0ac55a9ebcd3c10d7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-29 05:38:04 +0300

    number 1, added werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
+package {'Werkzeug':
+ensure   =>'2.1.1',
+provider =>'pip3',
+}
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
