06b3e176edb8b3c6b697411abe1bdbf36f317839
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-29 11:17:20 +0300

    changed where the file is

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 #config file changes
-file {'~/ssh/config':
+file {'/etc/ssh/ssh_config':
 ensure=>present,
 content=>"Host ubuntu\n\tHostName 54.236.30.220\n\tIdentityFile '~/.ssh/school'\n\tpasswordAuthentication no",
 }
