b6420b21985ff3f82abfb84a8c0a268db9f02364
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-01 09:29:37 +0300

    examples to understand how to go about number 1

---
+++ 0x0C-web_server/examples/1-example.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+echo "So whats your name"
+read my_name
+echo "nice to meet you $my_name"
+echo "have a nice day"
---
+++ 0x0C-web_server/examples/2-example.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+echo "argument: $1"
+echo "argument: $2"
+echo "argument: $3"
+
