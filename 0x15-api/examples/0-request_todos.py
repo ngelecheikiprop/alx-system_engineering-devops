ecf80d134b0136cdeb38feab3a1588c6d9fe5b07
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-07-29 10:45:18 +0300

    simple request for todos

---
+++ 0x15-api/examples/0-request_todos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'python script to get todo list progress'
+
+
+import requests
+
+"""
+user - 1
+todo - userid - 1 , id 1, id 2 - false/True
+
+for the employee id  - all todos - true/total
+display 
+
+urls =https://jsonplaceholder.typicode.com/users/1/todos - all todos for a user 1
+
+"""
+if __name__=="__main__":
+    import requests
+    url = 'https://jsonplaceholder.typicode.com/users/1/todos'
+    r = requests.get(url)
+    print(r.json())
